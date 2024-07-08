dacf9158c704045d2e7e69ef65cf1ee3c5fdd035
Siddhartha-Kolla <siddharthansg@gmail.com>
2024-07-08 21:48:19 +0200

    Changes the look of the mobile header version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import Container from './ui/container'
-// import { a } from 'react-router-dom'
-import { IoPersonCircleOutline} from "react-icons/io5";
-import { PiShoppingCart } from "react-icons/pi";
 import { Button } from "@/components/ui/button"
 import { LuShoppingCart } from "react-icons/lu";
 import { LuSearch } from "react-icons/lu";
@@ -105,41 +102,50 @@ const Header = () => {
               <SheetTrigger>
                 <LuMenu className="h-6 md:hidden w-6" />
               </SheetTrigger>
-              <SheetContent side="left" className="w-[300px] sm:w-[400px] bg-green-50">
-                <NavigationMenu>
-                  <NavigationMenuList className="bg-blue-100">
-                    <NavigationMenuItem>
-                      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                      <NavigationMenuContent>
-                        <NavigationMenuLink>Link</NavigationMenuLink>
-                      </NavigationMenuContent>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                          Documentation
-                        </NavigationMenuLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                      <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-                      <NavigationMenuContent>
-                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                          {components.map((component,index) => (
-                            <li key={component.title}>
-                              <NavigationMenuLink asChild>
-                                <a
-                                  href={component.href}
-                                  className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                                >
-                                  <div className="text-sm font-medium leading-none">{component.title}</div>
-                                </a>
-                              </NavigationMenuLink>
-                            </li>
-                          ))}
-                        </ul>
-                      </NavigationMenuContent>
-                    </NavigationMenuItem>
-                  </NavigationMenuList>
-                </NavigationMenu>
+              <SheetContent side="left" className="w-[300px] sm:w-[400px]">
+              <NavigationMenu className="mx-6 flex items-center space-x-4-lg:space-x-6 md:hidden flex-grow">
+                <NavigationMenuList className=" flex flex-col flex-grow w-full">
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger>Kategorien</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                        {components.map((component) => (
+                          <li key={component.title}>
+                            <NavigationMenuLink asChild className="group inline-flex h-9 w-full items-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-slate-100 hover:text-slate-900 focus:bg-slate-100 focus:text-slate-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-slate-100/50 data-[state=open]:bg-slate-100/50 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50 dark:focus:bg-slate-800 dark:focus:text-slate-50 dark:data-[active]:bg-slate-800/50 dark:data-[state=open]:bg-slate-800/50">
+                              <a
+                                href={component.href}
+                                className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                              >
+                                <div className="text-sm font-medium leading-none">{component.title}</div>
+                              </a>
+                            </NavigationMenuLink>
+                          </li>
+                        ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                      <a
+                        href="/"
+                        className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                      >
+                        <div className="text-sm font-medium leading-none">Reduziert</div>
+                      </a>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                      <a
+                        href="/"
+                        className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                      >
+                        <div className="text-sm font-medium leading-none">Über Uns</div>
+                      </a>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
               </SheetContent>
             </Sheet>
             <a href='/' className=' ml-4 lg:ml-0'>
@@ -165,4 +171,6 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
+
+// https://github.com/iorise/shopping-cart
